8dae7376d777dc6e19ec82b6064298bcd4151c71
Andrei Gheorghiu <gheorghiu.andrei@outlook.com>
2021-11-03 07:37:26 +0200

    add a primary bean controller

--- src/main/java/ro/pakt/javadi/JavaDiApplication.java
+++ src/main/java/ro/pakt/javadi/JavaDiApplication.java
@@ -16,8 +16,9 @@ public class JavaDiApplication {
 		ApplicationContext ctx = SpringApplication.run(JavaDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+
+		System.out.println("------ Primary Bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("------ Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
--- src/main/java/ro/pakt/javadi/controllers/MyController.java
+++ src/main/java/ro/pakt/javadi/controllers/MyController.java
@@ -2,12 +2,18 @@ package ro.pakt.javadi.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import ro.pakt.javadi.services.GreetingService;
+
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello World!!!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
